9b0303453b6d7ee3af70e1d742322462018a02e1
Sultanbek <118711803+sultik008@users.noreply.github.com>
2025-05-26 13:26:08 +0500

    Update route.js

--- Todo/routes/route.js
+++ Todo/routes/route.js
@@ -17,13 +17,12 @@ const router = express.Router();
 router.get("/", getAllAppeals);
 router.post("/", addAppeal);
 router.put("/cancelinprogress", cancelinprogressAppeal);
+router.get('/filter', GetbyDateAppeals); // пример отправки запроса | /appeals?startDate=2025-05-01&endDate=2025-05-25 диапозон дат || /appeals?date=2025-05-25 определенная дата 
 router.put("/:id", editAppeal);
 router.delete("/:id", deleteAppeal);
 router.put("/:id/cancel", cancelAppeal);
 router.put("/:id/start", startAppeal);
 router.put("/:id/complete", completeAppeal);
-
-
 
 
 export default router;
